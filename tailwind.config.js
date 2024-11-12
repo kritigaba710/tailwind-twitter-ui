d15eb710d600c820355060d293394f75d9e64dae
kritigaba710 <kritigaba31@gmail.com>
2024-11-12 05:57:00 -0800

    Initial commit of my project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: { colors: {
+      customColor: 'rgb(25, 26, 26)',
+      customGray:'rgb(82 79 79)',
+    }},
+  },
+  plugins: [],
+}
+
